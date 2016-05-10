820a1ebe4d37bc4251adc0752f851909220ac36f
Alex PC <alex@alexhawkes.com>
2016-05-10 14:37:51 +0100

    Created a new play page

---
+++ WC/WebContent/js/play/player.js
@@ -0,0 +1,55 @@
+var windowWidth = 0;
+var waveForm;
+var canvas1, context1;
+var touchDiv;
+var mp3Location = "./resources/originalUpload/";
+var audioTime;
+
+$(document).ready(function($) {
+	console.log("Loaded Play Page");
+	windowWidth = $(document).width();
+	windowHeight = $(document).height();
+	canvas1 = document.createElement('canvas');
+	canvas1.width = 850;
+	canvas1.height = 600;
+	canvas1.id = "canvas1";
+	context1 = canvas1.getContext('2d');
+	$('#canvasContainer').html(canvas1);
+	bindCanvasTouchControls();
+
+	loadTrack();
+});
+
+/*
+$(window).on('resize', function(){
+    var win = $(this); 
+    canvas1.width=win.width();
+    windowWidth=win.width();
+    waveForm.drawTime=calculateDrawTime();
+});
+
+
+*/
+
+function animate() {
+	requestAnimationFrame(animate);
+	audioTime = $("#audio").prop("currentTime") * 1000;
+	draw(audioTime);
+}
+
+function draw(time) {
+	context1.clearRect(0, 0, canvas1.width, canvas1.height);
+	$('#canvas1').css("background-color", $('#backgroundColor').val());
+	waveForm.draw(time, context1);
+}
+
+function loadTrack() {
+	var selectedText = $('#loadTrack').find(":selected").text();
+	var selectedValue = $('#loadTrack').find(":selected").val();
+	console.log(selectedValue);
+	var audio = document.getElementById('audio');
+	var source = document.getElementById('audioSrc');
+	source.src = mp3Location + selectedValue + ".mp3";
+	loadWaveForm(selectedValue);
+	audio.load(); 
+}
